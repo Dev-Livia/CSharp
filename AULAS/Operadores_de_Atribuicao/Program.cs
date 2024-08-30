4381a4a6960570a6d60b520c9ed98d47cb100911
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-08-30 18:19:14 -0300

    Create Program.cs

---
+++ AULAS/Operadores_de_Atribuicao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_de_Atribuicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            s += "DEF";
+            Console.WriteLine(s);   
+        }
+    }
+}
