22f54e14dfa27f30c914b198d7fb7fcd219f98e1
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-08-30 19:28:41 -0300

    Create Program.cs

---
+++ AULAS/Entrada de dados em C#/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrada_de_dados_em_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comando 
+            // Console.ReadLine();
+            //lê da entrada padrão até a quebra de linha.
+            //Retorna os dados lidos na forma de string.
+
+            Console.WriteLine("Olá, qual seu nome?");
+            string frase = Console.ReadLine();
+            Console.WriteLine($"Olá {frase}");
+
+            //Comando Split 
+            //string s = console.ReadLine();
+            //string[] vet = s.Split(' ');
+
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine($"{a},{b},{c}");
+
+        }
+    }
+}
