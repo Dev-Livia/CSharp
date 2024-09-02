bc91b263b1a7451ed3a3123f638b879727e9bae6
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-09-02 19:02:29 -0300

    up

---
+++ AULAS/Conversão implícita e casting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversão_implícita_e_casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            float b;
+            // Aqui estamos assumindo o risco de que talvez perderemos dados ao usar esta formula, pois um tem menos bytes que o outro.
+            a = 5.1;
+            b = (float)a;
+
+
+
+            
+        }
+    }
+}
