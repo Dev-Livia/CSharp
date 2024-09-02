efaf4a0910f70c54668e39bcf0a3090d94d3cf95
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-09-02 19:33:01 -0300

    Update Program.cs

--- AULAS/Operadores_Logicos/Program.cs
+++ AULAS/Operadores_Logicos/Program.cs
@@ -10,13 +10,17 @@ namespace Operadores_Logicos
     {
         static void Main(string[] args)
         {
-
+            //OPERADOR && AND
             bool c1 = 2 > 3 && 4 != 5;
             Console.WriteLine(c1);
 
+            //OPERADOR || OR
             bool c2 = 2 > 3 || 4 != 5;
             Console.WriteLine(c2);
 
+            //OPERADOR ! NEGAÇÃO
+            bool c3 = ! (2 > 3) || 4 != 5;
+            Console.WriteLine(c3);
         }
     }
 }
